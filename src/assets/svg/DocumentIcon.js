149e5643717c9053e26646941e87a667d092c9a1
Ichan Kabir <ichankabir@gmail.com>
2022-07-12 05:02:56 +0530

    Updates

--- src/assets/svg/DocumentIcon.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import pdfIcon from "../images/pdf.png";
-
-const DocumentIcon = () => {
-  return <img style={{ width: "4.6ch" }} src={pdfIcon} alt="pdf" />;
-};
-
-export default DocumentIcon;
--- src/utils/time.js
+++
@@ -1,5 +0,0 @@
-import moment from "moment";
-
-export const getCurrentTime = (date) => {
-  return moment(date).format("ddd MMM DD HH:mm:ss");
-};
