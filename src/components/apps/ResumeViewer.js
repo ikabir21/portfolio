90087d987bba474f448950a877bf19fedd4d1a98
Ichan Kabir <ichankabir@gmail.com>
2022-07-12 17:05:06 +0530

    resume update

--- src/components/apps/ResumeViewer.js
+++ src/components/apps/ResumeViewer.js
@@ -1,9 +1,10 @@
 import React from "react";
+import resume from "../../assets/documents/Ichan_Kabir_Resume.pdf";
 
 const ResumeViewer = () => {
   return (
     <iframe
-      src={"/Ichan_Kabir_Resume.pdf"}
+      src={resume}
       frameBorder="0"
       title="Ichan Kabir Resume"
       style={{ width: "100%", height: "100%" }}
